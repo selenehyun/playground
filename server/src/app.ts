532bc5f76f08620997282b4f0c512b1a542a2825
Tim <sh.kang@ecubelabs.com>
2022-04-08 14:22:10 +0900

    refactor: DI 사용법 2

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,8 +5,19 @@ import { UserService } from "./services/users/application/service";
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  const fakeUuid = `uuid-${Math.floor(Math.random() * 1000000)}`;
+
+  const container = Container.of(fakeUuid); // context 별로 격리된 컨테이너를 만들어 사용할 수 있다.
+  ctx.state.container = container; // ctx.state는 koa가 관리해주는 context에서 자유롭게 사용할 수 있는 변수
+
+  container.set("txId", fakeUuid);
+
+  await next();
+});
+
 app.use(async (ctx) => {
-  const service = Container.get(UserService); // 처음 컨테이너를 얻어올 때 컨테이너에 없으면 만들고, 있으면 그대로 다시 얻어온다.
+  const service = ctx.state.container.get(UserService); // 처음 컨테이너를 얻어올 때 컨테이너에 없으면 만들고, 있으면 그대로 다시 얻어온다.
 
   ctx.body = service.say("Hi!!");
 });
--- server/src/services/users/application/service.ts
+++ server/src/services/users/application/service.ts
@@ -1,13 +1,13 @@
-import { Service } from "typedi";
+import { Inject, Service } from "typedi";
 
 // Service 데코레이터를 통해 Container에 등록시킬수 있다.
 @Service()
 export class UserService {
-  constructor() {
-    console.log("user service가 생성됐다.");
+  constructor(@Inject("txId") private txId: string) {
+    console.log("user service가 생성됐다.", txId);
   }
 
   say(message: string) {
-    return message;
+    return `${message} - ${this.txId}`;
   }
 }
